cf74a3c322c79fa1dd31534f824358def91ab2b7
conradmazing <franciscoc1410@gmail.com>
2020-11-12 22:38:11 +0800

    Third commit, updated the splash screen part, with loaded animations already.

--- app/src/main/java/pwc/appdev/speedalert/Splash.java
+++ app/src/main/java/pwc/appdev/speedalert/Splash.java
@@ -2,16 +2,41 @@ package pwc.appdev.speedalert;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.Window;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Splash extends AppCompatActivity {
 
+    private static int SPLASH_SCREEN = 3500;
+    ImageView image;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Intent intent = new Intent(this, login.class);
-        startActivity(intent);
-        finish();
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.splash);
+
+        Animation animate = AnimationUtils.loadAnimation(this, R.anim.top_animation);
+        image = findViewById(R.id.welcomeimg);
+        image.setAnimation(animate);
+
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run() {
+                Intent intent = new Intent(Splash.this, login.class);
+                startActivity(intent);
+                finish();
+            }
+        }, SPLASH_SCREEN);
     }
 }
--- app/src/main/java/pwc/appdev/speedalert/login.java
+++ app/src/main/java/pwc/appdev/speedalert/login.java
@@ -1,6 +1,8 @@
 package pwc.appdev.speedalert;
 
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -9,6 +11,10 @@ public class login extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.login);
     }
 }
